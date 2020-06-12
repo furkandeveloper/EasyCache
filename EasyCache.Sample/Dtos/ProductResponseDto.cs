8ee9e9c608b5348ad5bcbea62f772639c719849f
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-06-12 23:20:39 +0300

    Add Response Dto for sample

---
+++ EasyCache.Sample/Dtos/ProductResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace EasyCache.Sample.Dtos
+{
+    public class ProductResponseDto
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
