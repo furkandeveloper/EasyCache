4830903f004c24c36497982ed45df46216b1c2d5
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-06-20 23:01:05 +0300

    Add Cache Service Extensions

---
+++ src/EasyCache/Helpers/Extensions/CacheServiceExtensions.cs
@@ -0,0 +1,36 @@
+﻿using EasyCache.Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyCache.Helpers.Extensions
+{
+    public static class CacheServiceExtensions
+    {
+        public static T GetAndSet<T>(this ICacheService cacheService, string key, Func<T> getResult,TimeSpan expireTime)
+        {
+            var data = cacheService.Get<T>(key);
+
+            if (data == null)
+            {
+                data = getResult();
+                cacheService.Set(key, data,expireTime);
+            }
+
+            return data;
+        }
+
+        public static T GetAndSetAsync<T>(this ICacheService cacheService, string key, Func<T> getResult, TimeSpan expireTime)
+        {
+            var data = cacheService.Get<T>(key);
+
+            if (data == null)
+            {
+                data = getResult();
+                cacheService.Set(key, data, expireTime);
+            }
+
+            return data;
+        }
+    }
+}
