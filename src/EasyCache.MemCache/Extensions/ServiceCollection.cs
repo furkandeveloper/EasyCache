09d586755ffec7f334594e35e80880d6ab176b16
furkandeveloper <furkan.dvlp@gmail.com>
2020-12-27 23:17:43 +0300

    🔨 Add ServiceCollection extension for EasyCache.MemCache

---
+++ src/EasyCache.MemCache/Extensions/ServiceCollection.cs
@@ -0,0 +1,32 @@
+﻿using EasyCache.Core.Abstractions;
+using EasyCache.MemCache.Concrete;
+using Enyim.Caching.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyCache.MemCache.Extensions
+{
+    /// <summary>
+    /// This class includes extension of ServiceCollection
+    /// </summary>
+    public static class ServiceCollection
+    {
+        /// <summary>
+        /// Add EasyMemCache to DI container.
+        /// </summary>
+        /// <param name="services">
+        /// Microsoft.Extensions.DependencyInjection.IServiceCollection
+        /// </param>
+        /// <returns>
+        /// Microsoft.Extensions.DependencyInjection.IServiceCollection
+        /// </returns>
+        public static IServiceCollection AddEasyMemCache(this IServiceCollection services, Action<MemcachedClientOptions> memcachedSettings)
+        {
+            services.AddEnyimMemcached(memcachedSettings);
+            services.AddTransient<IEasyCacheService, EasyCacheMemCacheManager>();
+            return services;
+        }
+    }
+}
